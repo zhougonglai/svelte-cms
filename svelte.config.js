01412bacd5c66d74d7131c276ba242cc2d985d60
周公来 <zhougonglai@nn.com>
2021-05-06 22:19:03 +0800

    adapter 重新适配

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import node from '@sveltejs/adapter-node';
-// const vercel = require('@sveltejs/adapter-vercel');
+// import node from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+export default {
 	preprocess: sveltePreprocess({ postcss: true }),
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		// adapter: node(),
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
-};
+}
